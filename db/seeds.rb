f68901f5a5e23861a93aa6521a2bbb6c60842e2a
Salanoid <salanoid@gmail.com>
2017-02-13 01:18:33 +0200

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+    SwotTable.create(title: 'test', strength: 'Selling products directly to the customers, Keeping costs below that of competitors, Higher responsiveness to customer demands', weakness: 'No partnerships or strong relationships with computer retailers', opportunity: 'Desire of customers for one-stop shopping, Customers’ increasing knowledge about what they want in computers, Internet as a marketing tool', threats: 'Stronger brand name of competitors like IBM and Compaq, Strong relationship of competitors with retailers')

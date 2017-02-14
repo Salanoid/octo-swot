f58e750225942009a753576351ece4d0d1d33dd4
Salanoid <salanoid@gmail.com>
2017-02-14 15:28:46 +0200

    foo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,13 +9,8 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"]
   }
   # Code is not reloaded between requests.
   config.cache_classes = true

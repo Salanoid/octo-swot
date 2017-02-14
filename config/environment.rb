0ea1e98a6246bc5c269cca8fc520aec1cebe2cd1
Salanoid <salanoid@gmail.com>
2017-02-14 14:43:34 +0200

    foo

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-config.action_mailer.default_url_options = { host: 'http://octo-swot.herokuapp.com/' }

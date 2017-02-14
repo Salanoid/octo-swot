e9ec5de6373460cc18922d17d86a1a6b082c20eb
Salanoid <salanoid@gmail.com>
2017-02-14 14:52:47 +0200

    foo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { :host => 'octo-swot.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'octo-swot.herokuapp.com' }
   # Code is not reloaded between requests.
   config.cache_classes = true
 

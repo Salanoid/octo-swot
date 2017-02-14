50d05b937ec08ac3b8d5d848b15d39bdb8068256
Salanoid <salanoid@gmail.com>
2017-02-14 12:06:39 +0200

    root

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :swot_tables
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/" => "swot_tables#index", :as => "root"
+  root 'swot_tables#index'
 end

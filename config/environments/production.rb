a32b293b67fec6ee4b7a57cdad6ce6c8aa5de4a2
Salanoid <salanoid@gmail.com>
2017-02-14 15:19:20 +0200

    foo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,15 +8,6 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
-  }
   # Code is not reloaded between requests.
   config.cache_classes = true
 

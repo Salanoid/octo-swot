44765264e28793a157d200896f58fe7680361072
Salanoid <salanoid@gmail.com>
2017-02-17 13:16:46 +0200

    unit tests

--- test/controllers/swot_tables_controller_test.rb
+++ test/controllers/swot_tables_controller_test.rb
@@ -3,6 +3,9 @@ require 'test_helper'
 class SwotTablesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @swot_table = swot_tables(:one)
+    @password = '123456789'
+    @confirmed_user = User.create(email: "#{rand(50000)}@example.com", 
+                              password: @password)
   end
 
   test "should get index" do
@@ -11,11 +14,13 @@ class SwotTablesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    sign_in(user: @confirmed_user, password: '123456789')
     get new_swot_table_url
     assert_response :success
   end
 
   test "should create swot_table" do
+    sign_in(user: @confirmed_user, password: '123456789')
     assert_difference('SwotTable.count') do
       post swot_tables_url, params: { swot_table: { opportunity: @swot_table.opportunity, strength: @swot_table.strength, threats: @swot_table.threats, title: @swot_table.title, weakness: @swot_table.weakness } }
     end
@@ -24,21 +29,25 @@ class SwotTablesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should show swot_table" do
+    sign_in(user: @confirmed_user, password: '123456789')
     get swot_table_url(@swot_table)
     assert_response :success
   end
 
   test "should get edit" do
+    sign_in(user: @confirmed_user, password: '123456789')
     get edit_swot_table_url(@swot_table)
     assert_response :success
   end
 
   test "should update swot_table" do
+    sign_in(user: @confirmed_user, password: '123456789')
     patch swot_table_url(@swot_table), params: { swot_table: { opportunity: @swot_table.opportunity, strength: @swot_table.strength, threats: @swot_table.threats, title: @swot_table.title, weakness: @swot_table.weakness } }
     assert_redirected_to swot_table_url(@swot_table)
   end
 
   test "should destroy swot_table" do
+    sign_in(user: @confirmed_user, password: '123456789')
     assert_difference('SwotTable.count', -1) do
       delete swot_table_url(@swot_table)
     end

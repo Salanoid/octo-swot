1f30fe52ff96e6f76352d599391e80cd7f35900d
Salanoid <salanoid@gmail.com>
2017-02-17 13:53:59 +0200

    simple user test model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid user' do
+    user = User.new(email: 'john@example.com', password: '123456789')
+    assert user.valid?
+  end
+
+  test 'invalid without email' do
+    user = User.new(password: '123456789')
+    refute user.valid?
+    assert_not_nil user.errors[:email]
+  end
 end
